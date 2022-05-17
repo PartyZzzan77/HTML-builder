263232f7f5afe302f65b20780948b43e2b1f67f7
Mitya <partyzan327@gmail.com>
2022-05-17 19:41:59 +0300

    fix: add stream buffering for recording

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,12 +5,14 @@ const PARTING = 'See you soon! ';
 
 const output = fs.createWriteStream(path.join(__dirname, 'write-file.txt'));
 
-stdout.write('Please enter your message\n(to exit use exit or cmd+C)\n');
+stdout.write('Please enter your message\n(to exit use exit or control + C)\n');
 
 stdin.on('data', (data) => {
   let msg = data.toString().trim();
   if (msg.toLowerCase() !== 'exit') {
+    output.cork();
     output.write(`${msg.trim()} `);
+    output.uncork();
   } else {
     stdout.write(PARTING);
     exit();
