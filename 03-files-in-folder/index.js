f94b9e24e796616dacf0943632cdd5fe455420e7
Mitya <partyzan327@gmail.com>
2022-05-17 17:28:25 +0300

    fix: rewrite to async await syntax /add error handling

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,29 +2,32 @@ const fs = require('fs');
 const path = require('path');
 const { readdir } = require('fs/promises');
 const { stdout } = process;
+
 const TARGET_PATH = path.join(__dirname, 'secret-folder');
 
-fs.stat(TARGET_PATH, (err, stats) => {
-  if (err) throw new Error(err.message);
-  if (stats.isDirectory()) {
-    readdir(TARGET_PATH, { withFileTypes: true })
-      .then((resolve) => {
-        resolve.forEach((file) => {
-          if (file.isFile()) {
-            const filePath = path.join(TARGET_PATH, file.name);
-            const fileParams = path.parse(filePath);
+fs.stat(TARGET_PATH, async (err, stats) => {
+  try {
+    if (err) throw err;
+    else if (stats.isDirectory()) {
+      const dir = await readdir(TARGET_PATH, { withFileTypes: true });
+      for await (const file of dir) {
+        if (file.isFile()) {
+          const filePath = path.join(TARGET_PATH, file.name);
+          const fileParams = path.parse(filePath);
 
-            fs.stat(filePath, (err, stats) => {
-              if (err) throw new Error(err);
-              stdout.write(
-                `${fileParams.name} - ${fileParams.ext.slice(1)} - ${(
-                  stats.size / 1024
-                ).toFixed(3)}kb\n`
-              );
-            });
-          }
-        });
-      })
-      .catch((err) => stdout.write(`ERROR: ${err.message}`));
+          fs.stat(filePath, (err, stats) => {
+            if (err) throw err;
+
+            stdout.write(
+              `${fileParams.name} - ${fileParams.ext.slice(1)} - ${(
+                stats.size / 1024
+              ).toFixed(3)}kb\n`
+            );
+          });
+        }
+      }
+    }
+  } catch (err) {
+    console.log('Error: ', err.message);
   }
 });
