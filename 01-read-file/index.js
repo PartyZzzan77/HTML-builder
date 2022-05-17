34378f861ec09b45b400e47d2c40c67dcafd06f3
Mitya <partyzan327@gmail.com>
2022-05-17 19:12:51 +0300

    fix: add a pipe method to merge streams

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const { stdout } = process;
+
 const readableStream = fs.createReadStream(
   path.join(__dirname, 'text.txt'),
   'utf-8'
 );
-readableStream.on('data', (chunk) => stdout.write(chunk));
+readableStream.pipe(stdout);
