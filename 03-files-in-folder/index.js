147d12cbedc822fb2a3d5ec6ce64b55a519e5225
Mitya <partyzan327@gmail.com>
2022-05-17 18:00:06 +0300

    fix: rewrite to async await syntax function stat

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,33 +1,29 @@
-const fs = require('fs');
 const path = require('path');
-const { readdir } = require('fs/promises');
+const { readdir, stat } = require('fs/promises');
 const { stdout } = process;
 
 const TARGET_PATH = path.join(__dirname, 'secret-folder');
 
-fs.stat(TARGET_PATH, async (err, stats) => {
+(async () => {
   try {
-    if (err) throw err;
-    else if (stats.isDirectory()) {
+    const stats = await stat(TARGET_PATH);
+    if (stats.isDirectory()) {
       const dir = await readdir(TARGET_PATH, { withFileTypes: true });
       for await (const file of dir) {
         if (file.isFile()) {
           const filePath = path.join(TARGET_PATH, file.name);
           const fileParams = path.parse(filePath);
 
-          fs.stat(filePath, (err, stats) => {
-            if (err) throw err;
-
-            stdout.write(
-              `${fileParams.name} - ${fileParams.ext.slice(1)} - ${(
-                stats.size / 1024
-              ).toFixed(3)}kb\n`
-            );
-          });
+          const filesStat = await stat(filePath);
+          stdout.write(
+            `${fileParams.name} - ${fileParams.ext.slice(1)} - ${(
+              filesStat.size / 1024
+            ).toFixed(3)}kb\n`
+          );
         }
       }
     }
   } catch (err) {
     console.log('Error: ', err.message);
   }
-});
+})();
