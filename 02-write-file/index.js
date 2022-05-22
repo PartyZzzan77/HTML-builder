0cf8ca198ed6dea1af34646414bc65a26d191e2f
Mitya <partyzan327@gmail.com>
2022-05-22 13:30:25 +0300

    fix: remove the % sign in the output

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { stdin, stdout, exit } = process;
-const PARTING = 'See you soon! ';
+const PARTING = 'See you soon!';
 
 const output = fs.createWriteStream(path.join(__dirname, 'write-file.txt'));
 
@@ -10,16 +10,14 @@ stdout.write('Please enter your message\n(to exit use exit or control + C)\n');
 stdin.on('data', (data) => {
   let msg = data.toString().trim();
   if (msg.toLowerCase() !== 'exit') {
-    output.cork();
     output.write(`${msg.trim()}\n`);
-    output.uncork();
   } else {
-    stdout.write(PARTING.trim());
+    stdout.write(`${PARTING}\n`);
     exit();
   }
 });
 
 process.on('SIGINT', () => {
-  stdout.write(PARTING.trim());
+  stdout.write(`${PARTING}\n`);
   exit();
 });
